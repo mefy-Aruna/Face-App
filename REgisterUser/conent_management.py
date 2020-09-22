14bb8014925e81a2b6167113d32f79ae4311d937
mefy-Aruna <71429380+mefy-Aruna@users.noreply.github.com>
2020-09-23 00:10:21 +0530

    Add files via upload

---
+++ REgisterUser/conent_management.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 19:19:35 2020
+
+@author: Aruna
+"""
+
---
+++ REgisterUser/content_management.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 19:19:35 2020
+
+@author: Aruna
+"""
+
+def Content():
+    TOPIC_DICT = {"Basics":[["Introduction to Python","/introduction-to-python-programming/"],
+                            ["Print functions and Strings","/python-tutorial-print-function-strings/"],
+                            ["Math basics with Python 3","/math-basics-python-3-beginner-tutorial/"]],
+                  "Web Dev":[]}
+
+    return TOPIC_DICT
